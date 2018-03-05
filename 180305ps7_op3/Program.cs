b07e765ead711b1b64eef0da483af71d0a64c54b
junho_choi <singa34@naver.com>
2018-03-05 12:35:48 +0900

    초기 커밋.

---
+++ 180305ps7_op3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180305ps6_io1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strName;
+            Console.Write("당신의 이름은 ? ");
+            strName = Console.ReadLine();
+
+            Console.WriteLine("Hello, {0}!",strName);
+        }
+    }
+}
